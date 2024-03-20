fd57901ecb488a37f158234fda8bce664ff6b8f6
chychey <greenchy222@gmail.com>
2024-03-20 12:16:34 -0700

    commit

--- myConsoleProjects.cs/myConsoleProjects.cs/Program.cs
+++ myConsoleProjects.cs/myConsoleProjects.cs/Program.cs
@@ -20,16 +20,21 @@ namespace myConsoleProjects.cs
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics");
             string posExperience = Console.ReadLine();
             Console.WriteLine("What page number?");
-            string pageNumber = Console.ReadLine();
+
+            
+            int pageNumber = Convert.ToInt32(Console.ReadLine());   
+            //string pageNumber = Console.ReadLine();
+            //int pageNumberInt = Convert.ToInt32(pageNumber);
 
             Console.WriteLine("Do you need help with anything? Please answer True or False");
-            string yesNo = Console.ReadLine();
+            bool yesNo = Convert.ToBoolean(Console.ReadLine());
+            
 
             Console.WriteLine("Is there any other feedback? Please be specific");
             string feedBack = Console.ReadLine();
 
             Console.WriteLine("How many hours did you study?");
-            string studyHour = Console.ReadLine();
+            int studyHour = Convert.ToInt32 (Console.ReadLine());
 
             Console.WriteLine("Thank you for your answers!"); //ends console//
             string thankYou = Console.ReadLine();
