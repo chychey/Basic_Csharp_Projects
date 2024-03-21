1a0d011276e34e9f10ef2b91950869f661f9e46f
chychey <greenchy222@gmail.com>
2024-03-21 13:33:27 -0700

    commit

---
+++ ShippingProject3/ShippingProject3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingProject3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your package weight?");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("This package is too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+
+
+
+                Console.WriteLine("What is the package width?");
+                int width = Convert.ToInt32(Console.ReadLine());
+                
+
+
+                Console.WriteLine("What is the package length?");
+                int length = Convert.ToInt32(Console.ReadLine());
+                
+
+                Console.WriteLine("What is the package heigth?");
+                int heigth = Convert.ToInt32(Console.ReadLine());
+              
+
+
+                int totalDimensions = (length + heigth + width);
+                if (totalDimensions > 50)
+                {
+                    Console.WriteLine("Dimensions are too big to be shipped via Package Express.");
+                    }
+                else
+                {
+                    int quote = (length * heigth * width) * packageWeight / 100;
+                    Console.WriteLine("$" + quote + " This is your total cost of shipping");
+                }
+
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+       
---
+++ shippingproject/Shipping1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your package weight?");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (packageWeight > 50)
+            {
+                ("This package is too heavy to ship.")
+            }
+            else
+            {
+
+
+
+                Console.WriteLine("What is the package width?");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(width);
+
+
+                Console.WriteLine("What is the package length?");
+                int length = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(length);
+                Console.ReadLine();
+
+                Console.WriteLine("What is the package heigth?");
+                int heigth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(heigth);
+
+
+                int totalDimensions(length +heigth + width);
+                if (totalDimensions > 50)
+                {
+                    ("Dimensions are too big to ship.")
+                    }
+                else
+                {
+                    int quote = (length * heigth * width) * packageWeight / 100;
+                    Console.WriteLine("$" + quote + " This is your total cost of shipping");
+                }
+                
+            }
+            
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
