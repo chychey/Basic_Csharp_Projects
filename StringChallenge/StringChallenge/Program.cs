b6a7625a773c55d79e1e4f710e0992349cf6e478
chychey <greenchy222@gmail.com>
2024-03-21 21:42:18 -0700

    commit

---
+++ StringChallenge/StringChallenge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "<Type name here>";
+            username = username.ToUpper();
+            string dataString = DateTime.Today.ToShortDateString();
+            
+
+            string str = "Hello " + username + ". Today is " + dataString + ".";
+            System.Console.WriteLine(str);
+
+            str += " How are you today?";
+            System.Console.WriteLine(str);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("I said, \"Hello\", Chy. \n I am well. \n \t How about you?. \n I love life. " );
+            
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringFunction/StringFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Chy";
+            //string quote = "I said, \"Hello\", Chy. \n Hello on a new line. \n \t Hello on a tab.";
+            //string fileName = @"C:Users\Chy";
+
+            //bool trueOrfalse = name.Contains("s");
+            //trueOrfalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Chy");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
