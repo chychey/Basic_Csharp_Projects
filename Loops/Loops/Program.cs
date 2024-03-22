52916f3dedb20f0df022da730b20e071d24cdc97
chychey <greenchy222@gmail.com>
2024-03-21 19:49:56 -0700

    commit

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my favorite number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 222;
+
+            do
+            {
+                switch (number)
+                {
+                    case 5:
+                        Console.WriteLine("You guessed 5. Try again.");
+                        Console.WriteLine("Guess again?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("You guessed 4. Try again.");
+                        Console.WriteLine("Guess again?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("You guessed 3. Try again.");
+                        Console.WriteLine("Guess again?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 222:
+                        Console.WriteLine("You gussed the number 222. That is my favorite number!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Nope.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.Read());
+                        break;
+                }
+
+            }
+            while (!isGuessed);
+            
+
+            Console.Read();
+        }
+    }
+}
