6f3b73b3376cce132ace18f6d6dea426f917554f
chychey <greenchy222@gmail.com>
2024-03-25 14:39:21 -0700

    commit

--- ConsoleAppProject/ConsoleAppProject/Program.cs
+++ ConsoleAppProject/ConsoleAppProject/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleAppProject
     {
         static void Main(string[] args)
         {
+            //part one
             string[] drinks = { "Soda", "Juice", "Tea" };
-            Console.WriteLine(drinks);
             Console.WriteLine("Please enter some text");
             string userinput = Console.ReadLine();
             for (int i = 0; i < drinks.Length; i++)
@@ -37,13 +37,91 @@ namespace ConsoleAppProject
             {
                 Console.WriteLine("Out of range.");
             }
+            //part 2
             while (true)
             {
-                //if (true)
+                if (true)
                 Console.WriteLine("What is your favorite drink?");
 
-                //break; 
+                break; 
             }
+            //part three
+            for (int i = 0; i < 3; i++)
+			{
+                Console.WriteLine(i);
+
+			}
+            for (int i = 0; i <= 3; i++)
+			{
+                Console.WriteLine(i);
+
+			}
+            //part 4
+            List<string> aList = new List<string>() { "truck", "pig", "shoes", "baseball"};
+            Console.WriteLine("Enter text to seach for something in the list");
+            string userinput2 = Console.ReadLine();
+            for (int i = 0; i < aList.Count; i++)
+            {
+                if (aList.Contains(userinput2)) { 
+                if(userinput2 == aList[i])
+                    {
+                    Console.WriteLine(aList[i] + " index is " + i);
+                        break;
+                }
+                }
+                else
+                {
+                    Console.WriteLine("users input is not on the list.");
+                }
+            }
+
+            //part 5
+             List<string> aList2 = new List<string>() { "truck", "pig", "shoes", "pig", "baseball"};
+            Console.WriteLine("Enter text to seach for something in the list");
+            string userinput3 = Console.ReadLine();
+            for (int i = 0; i < aList2.Count; i++)
+            {
+                if (aList.Contains(userinput3)) { 
+                if(userinput3 == aList2[i])
+                    {
+                    Console.WriteLine(aList2[i] + " index is " + i);
+                        
+                }
+                }
+                else
+                {
+                    Console.WriteLine("users input is not on the list.");
+                }
+            }
+            //part 6
+            List<string> aList3 = new List<string>() { "truck", "pig", "shoes", "pig", "baseball"};
+            List<string> dupList = new List<string>();
+            foreach (var item in aList3)
+	        {
+                if (dupList.Contains(item))
+                {
+                    Console.WriteLine(item + " is a duplicate");
+                }
+                else
+                {
+                    Console.WriteLine(item + " is unique");
+                    dupList.Add(item);
+                }
+
+
+	        }
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            Console.ReadLine();
+                    
 
         }
     }
