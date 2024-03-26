a54a2c5592abbaa9bdf94f6a580b811508802f18
chychey <greenchy222@gmail.com>
2024-03-26 11:57:40 -0700

    commit

---
+++ Callingmethods/Callingmethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callingmethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number to do math operations on.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(methods.methods1(numberOne));
+            Console.WriteLine(methods.methods2(numberOne));
+            Console.WriteLine(methods.methods3(numberOne));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Callingmethods/Callingmethods/methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callingmethods
+{
+    public static class methods
+    {
+        public static int methods1(int problem) //int problem returns math by user. 
+        {
+            return problem + 5;
+        }
+        public static int methods2(int problem)
+        {
+            return problem / 2;
+        }
+        public static int methods3(int problem)
+        {
+            return problem - 7;
+        }
+
+
+    }
+}
