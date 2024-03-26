ebb4f3944d9a5abee7d20e5fde3a3cb693fcc908
chychey <greenchy222@gmail.com>
2024-03-26 15:16:49 -0700

    commit

---
+++ MethodsAssigment/MethodsAssigment/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssigment
+{
+    public class Class1
+    {
+        public int methods1(int problem, int problem2 = 3)// two integers as parameters, default if user does not put second number is 3
+        {
+            return problem + problem2; //math operation from user input
+            
+        }
+        public int methods2(int problem)//extra code that is not used in this assignment
+        {
+            return problem - 2;
+        }
+    }
+}
---
+++ MethodsAssigment/MethodsAssigment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 mathOp = new Class1(); //instantiate class
+            Console.WriteLine("Input two numbers, one at a time. You do not need to enter anything for the second number");
+            int numberOne = Convert.ToInt32(Console.ReadLine()); //creating input for user to execute the integer
+            try //putting option to solve both numbers user input or default to first number they input
+            {
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(mathOp.methods1(numberOne, numberTwo));//calling class (mathOp) and method(method1), + getting user input(numberOne,numberTwo)
+            }
+            catch
+            {
+                Console.WriteLine(mathOp.methods1(numberOne));//if user does not put in second number this catches it and solves (numberOne)
+            }
+            Console.ReadLine();  
+        }
+        
+       
+    }
+}
