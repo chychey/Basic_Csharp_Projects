d2ac9a04c25034d726cba0fde2b5885bc81e41e1
chychey <greenchy222@gmail.com>
2024-03-27 22:13:54 -0700

    commit

---
+++ AbstractAssignment/AbstractAssignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssignment
+{
+    class Employee : Person //Derived class (employee) Inheriting from person
+    {
+        //implementing the SayName method
+        public override void SayName()
+        {
+            Console.WriteLine($"Emplyee's name: {FirstName} {LastName}");
+        }
+    }
+}
---
+++ AbstractAssignment/AbstractAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee //Instantiate an Employee object
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+            };
+
+            employee.SayName(); //Call SayName method
+
+        }
+
+    }
+
+}
+        
+
+
+    
+
